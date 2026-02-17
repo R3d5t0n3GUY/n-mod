707c62b6c16bf34e2043039e6107a0179b8e19fc
R3d5t0n3GUY <222323123+R3d5t0n3GUY@users.noreply.github.com>
2026-02-16 19:46:04 -0800

    quick bug fix

--- js/entities/bullet.js
+++ js/entities/bullet.js
@@ -9617,7 +9617,7 @@ const b = {
             for (let i = 0, len = b.inventory.length; i < len; ++i) {
               if (b.guns[b.inventory[i]].name === "scythe" && tech.durabilityScythe) {
                 let duraBoost = ((tech.isAmmoForGun && b.guns[b.activeGun].name === 'scythe') ? 30 : 15);
-                tech.forEach(t => {
+                tech.tech.forEach(t => {
                   if (t.name === "marginal utility") {
                     if (b.guns[t.gun].name === 'scythe') duraBoost *= Math.pow(2, t.count)
                   }
