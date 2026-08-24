3e67fc3bc305b8d827046cee969103e50757f318
R3d5t0n3GUY <222323123+R3d5t0n3GUY@users.noreply.github.com>
2026-08-23 22:41:34 -0700

    intdentation

--- js/loading/init.js
+++ js/loading/init.js
@@ -1,54 +1,54 @@
 setTimeout(() => {
-    fileLoads.isInitJS = true; //for file handling in fileTester.js
+	fileLoads.isInitJS = true; //for file handling in fileTester.js
 }, 10);
 
 const tryRunning = {
-    start() {
-        try {
-            simulation.startGame()
-        } catch (error) {
-            let text = `Uncaught ${error.name} in startGame(): `;
-            text += error.message;
-            text += "\nReload the page to try again.";
-            document.body.style.backgroundColor = "white";
-            favIcon.href = 'img/Error.png';
-            document.title = "n-mod: UNCAUGHT ERROR";
-            //document.body.innerHTML = text;
-            startBtn.style.cursor = "not-allowed"
-            console.error(error)
-            window.alert(text)
-        }
-    },
-    training() {
-        try {
-            simulation.startGame(false, true)
-        } catch (error) {
-            let text = `Uncaught ${error.name} in startTraining(): `;
-            text += error.message;
-            text += "\nReload the page to try again.";
-            document.body.style.backgroundColor = "white";
-            favIcon.href = 'img/Error.png';
-            document.title = "n-mod: UNCAUGHT ERROR";
-            //document.body.innerHTML = text;
-            trainBtn.style.cursor = "not-allowed"
-            console.error(error)
-            window.alert(text)
-        }
-    },
-    experiment() {
-        try {
-            build.openExperimentMenu()
-        } catch (error) {
-            let text = `Uncaught ${error.name} in openExperimentMenu(): `;
-            text += error.message;
-            text += "\nReload the page to try again.";
-            document.body.style.backgroundColor = "white";
-            favIcon.href = 'img/Error.png';
-            document.title = "n-mod: UNCAUGHT ERROR";
-            //document.body.innerHTML = text;
-            experimentBtn.style.cursor = "not-allowed"
-            console.error(error)
-            window.alert(text)
-        }
-    }
+	start() {
+		try {
+			simulation.startGame()
+		} catch (error) {
+			let text = `Uncaught ${error.name} in startGame(): `;
+			text += error.message;
+			text += "\nReload the page to try again.";
+			document.body.style.backgroundColor = "white";
+			favIcon.href = 'img/Error.png';
+			document.title = "n-mod: UNCAUGHT ERROR";
+			//document.body.innerHTML = text;
+			startBtn.style.cursor = "not-allowed"
+			console.error(error)
+			window.alert(text)
+		}
+	},
+	training() {
+		try {
+			simulation.startGame(false, true)
+		} catch (error) {
+			let text = `Uncaught ${error.name} in startTraining(): `;
+			text += error.message;
+			text += "\nReload the page to try again.";
+			document.body.style.backgroundColor = "white";
+			favIcon.href = 'img/Error.png';
+			document.title = "n-mod: UNCAUGHT ERROR";
+			//document.body.innerHTML = text;
+			trainBtn.style.cursor = "not-allowed"
+			console.error(error)
+			window.alert(text)
+		}
+	},
+	experiment() {
+		try {
+			build.openExperimentMenu()
+		} catch (error) {
+			let text = `Uncaught ${error.name} in openExperimentMenu(): `;
+			text += error.message;
+			text += "\nReload the page to try again.";
+			document.body.style.backgroundColor = "white";
+			favIcon.href = 'img/Error.png';
+			document.title = "n-mod: UNCAUGHT ERROR";
+			//document.body.innerHTML = text;
+			experimentBtn.style.cursor = "not-allowed"
+			console.error(error)
+			window.alert(text)
+		}
+	}
 }
